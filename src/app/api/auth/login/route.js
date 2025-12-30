e68dad696ab86a31a18b3320d354bc5b3214eff9
nawalnajam <nawalnajam80@gmail.com>
2025-12-30 14:14:41 +0500

    final build fix

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect";
+import dbConnect from "@/lib/db";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
